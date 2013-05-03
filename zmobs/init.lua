b7c7c926449524821ba9d5cbe99414dc9810693c
Zeg9 <dazeg9@gmail.com>
2013-05-03 16:29:36 +0200

    Add lava flan

--- zmobs/init.lua
+++ zmobs/init.lua
@@ -71,3 +71,52 @@ mobs:register_arrow("zmobs:mese_arrow", {
 	end
 })
 
+
+mobs:register_mob("zmobs:lava_flan", {
+	type = "monster",
+	hp_max = 10,
+	collisionbox = {-0.5, -0.5, -0.5, 0.5, 1.5, 0.5},
+	visual = "mesh",
+	mesh = "zmobs_lava_flan.x",
+	textures = {"zmobs_lava_flan.png"},
+	visual_size = {x=1, y=1},
+	makes_footstep_sound = true,
+	view_range = 10,
+	walk_velocity = 0.5,
+	run_velocity = 2,
+	damage = 3,
+	drops = {
+		{name = "bucket:bucket_lava",
+		chance = 1,
+		min = 1,
+		max = 1,},
+	},
+	light_resistant = true,
+	armor = 80,
+	drawtype = "front",
+	water_damage = 1,
+	lava_damage = 0,
+	light_damage = 0,
+	attack_type = "dogfight",
+	animation = {
+		speed_normal = 15,
+		speed_run = 15,
+		stand_start = 0,
+		stand_end = 8,
+		walk_start = 10,
+		walk_end = 18,
+		run_start = 20,
+		run_end = 28,
+		punch_start = 20,
+		punch_end = 28,
+	}
+})
+mobs:register_spawn("zmobs:lava_flan", {"default:lava_source"}, 15, -1, 1000, 10, 0)
+
+
+-- TODO
+-- Make special drops for lava flan
+-- Register eggs for every mob, and make them drop it occasionally
+
+
+
