52936067d8a4d18ac7a5c434c548329e9dc990b3
Zeg9 <dazeg9@gmail.com>
2013-05-03 19:15:22 +0200

    Add eggs, mobs will randomly drop them; add lava orb, to be used later, and change drop rates.

--- zmobs/init.lua
+++ zmobs/init.lua
@@ -2,6 +2,38 @@
 -- Released under WTFPL for code,
 --  CC BY-SA 3.0 for data (models and textures)
 --  see README.txt for more information.
+
+-- API (only about registering eggs, but never mind)
+zmobs = {}
+function zmobs:register_egg(modname, mobname, mobdesc)
+	minetest.register_craftitem(modname..":egg_"..mobname, {
+		description = mobdesc.." egg",
+		inventory_image = modname.."_egg_"..mobname..".png",
+		on_place = function(itemstack, placer, pointed_thing)
+			minetest.env:add_entity(pointed_thing.above, modname..":"..mobname)
+			if not minetest.setting_getbool("creative_mode") then
+				itemstack:take_item()
+			end
+			return itemstack
+		end,
+	})
+end
+
+-- Items
+
+minetest.register_craftitem("zmobs:lava_orb", {
+	description = "Lava orb",
+	inventory_image = "zmobs_lava_orb.png",
+	on_place = function(itemstack, placer, pointed_thing)
+		minetest.item_place_node(ItemStack({name="fire:basic_flame"}), placer, pointed_thing)
+		if not minetest.setting_getbool("creative_mode") then
+			itemstack:take_item()
+		end
+		return itemstack
+	end,
+})
+
+-- Mobs
 mobs:register_mob("zmobs:mese_monster", {
 	type = "monster",
 	hp_max = 10,
@@ -17,17 +49,21 @@ mobs:register_mob("zmobs:mese_monster", {
 	damage = 3,
 	drops = {
 		{name = "default:mese",
-		chance = 2,
-		min = 0,
-		max = 2,},
+		chance = 9,
+		min = 1,
+		max = 1,},
 		{name = "default:mese_crystal",
-		chance = 2,
-		min = 0,
-		max = 5,},
+		chance = 9,
+		min = 1,
+		max = 3,},
 		{name = "default:mese_crystal_fragment",
-		chance = 2,
-		min = 0,
-		max = 10,},
+		chance = 1,
+		min = 1,
+		max = 9,},
+		{name = "zmobs:egg_mese_monster",
+		chance = 3,
+		min = 1,
+		max = 1,},
 	},
 	light_resistant = true,
 	armor = 80,
@@ -52,6 +88,7 @@ mobs:register_mob("zmobs:mese_monster", {
 	}
 })
 mobs:register_spawn("zmobs:mese_monster", {"default:stone"}, 3, -1, 5000, 3, 0)
+zmobs:register_egg("zmobs","mese_monster","Mese monster")
 
 mobs:register_arrow("zmobs:mese_arrow", {
 	visual = "sprite",
@@ -86,15 +123,19 @@ mobs:register_mob("zmobs:lava_flan", {
 	run_velocity = 2,
 	damage = 3,
 	drops = {
-		{name = "bucket:bucket_lava",
-		chance = 1,
+		{name = "zmobs:lava_orb",
+		chance = 25,
+		min = 1,
+		max = 1,},
+		{name = "zmobs:egg_lava_flan",
+		chance = 2,
 		min = 1,
 		max = 1,},
 	},
 	light_resistant = true,
 	armor = 80,
 	drawtype = "front",
-	water_damage = 1,
+	water_damage = 5,
 	lava_damage = 0,
 	light_damage = 0,
 	attack_type = "dogfight",
@@ -112,6 +153,7 @@ mobs:register_mob("zmobs:lava_flan", {
 	}
 })
 mobs:register_spawn("zmobs:lava_flan", {"default:lava_source"}, 15, -1, 1000, 10, 0)
+zmobs:register_egg("zmobs","lava_flan","Lava flan")
 
 
 -- TODO
